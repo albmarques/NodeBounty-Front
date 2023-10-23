5bc09050ff115ec688ccd3f3cb85b3382971d104
albmarques <alberto.abrao@fatec.sp.gov.br>
2023-10-23 18:40:33 -0300

    toggle

--- src/components/CreditCard/index.jsx
+++ src/components/CreditCard/index.jsx
@@ -1,20 +1,30 @@
 import styles from './styles.module.css'
-
+import eye from '@assets/olho.png';
+const mascara= "****";
+function usarMascara(){
+  
+}
 export function CreditCard({ numeroCartao, validadeCartao, cvcCartao }) {
   return (
     <div class={" container mt-5"}>
       <div class="row justify-content-center">
         <div class="col-lg-4 col-md-8 col-sm-10 col-12">
           <div class={styles.cardContainer}>
+            <div class={styles.cardContent}>
             <div class={styles.cardNumber}>
-              {numeroCartao.substr(0, 4) +
+              
+              {mascara/*numeroCartao.substr(0, 4)*/ +
                 " " +
-                numeroCartao.substr(4, 4) +
+               mascara /*numeroCartao.substr(4, 4)*/ +
                 " " +
                 numeroCartao.substr(8, 4) +
                 " " +
                 numeroCartao.substr(12, 4)}
-            </div>
+              </div>
+                 <img class={styles.eye}src={eye} />
+                 </div>
+            
+           
             <div class={styles.expirationCvv}>
               <div>
                 <div>Data Exp.</div>
