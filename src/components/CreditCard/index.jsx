e3fd1f9eb89b11f176c1d5262f54c52a77f24d13
darkmetak <matporto03@gmail.com>
2023-10-24 10:37:22 -0300

    fixes

--- src/components/CreditCard/index.jsx
+++ src/components/CreditCard/index.jsx
@@ -6,11 +6,19 @@ export function CreditCard({ numeroCartao, validadeCartao, cvcCartao }) {
       <div className="row justify-content-center">
         <div className="col-lg-6 col-md-8 col-sm-10 col-12">
           <div className={styles.cardContainer}>
-            <div className={styles.cardNumber}>{numeroCartao}</div>
+            <div className={styles.cardNumber}>
+              {numeroCartao.substr(0, 4) +
+                ' ' +
+                numeroCartao.substr(4, 4) +
+                ' ' +
+                numeroCartao.substr(8, 4) +
+                ' ' +
+                numeroCartao.substr(12, 4)}
+            </div>
             <div className={styles.expirationCvv}>
               <div>
                 <div>Data Exp.</div>
-                <div>{String(validadeCartao)}</div>
+                <div>{validadeCartao.substr(5, 5)}</div>
               </div>
               <div>
                 <div>CVV</div>
